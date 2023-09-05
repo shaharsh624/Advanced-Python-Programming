48c4e3ac07acd400288cd20b3a503bde78410195
Harsh Shah <harshdev624@gmail.com>
2023-09-05 08:07:33 +0530

    Completed task

--- UNT 2/pyautogui/main.py
+++ UNT 2/pyautogui/main.py
@@ -1,16 +1,37 @@
-# Screen size: 1920x1080
-# Google Docs - right side
-# Code editor - left side
-
-import pyautogui
+import pyautogui as p
 import time
 
-copy_x, copy_y = 300, 300
-pyautogui.click(copy_x, copy_y)
-pyautogui.hotkey('ctrl', 'a')
-pyautogui.hotkey('ctrl', 'c')
-paste_x, paste_y = 1200, 500
-pyautogui.click(paste_x, paste_y)
-pyautogui.hotkey('ctrl', 'shift', 'v')
+copy_stite = r"C:/Users/harsh/OneDrive - pdpu.ac.in/HARSH/_STUDY MATERIAL/SEM 5/Advanced Python Programming/UNT 2/pyautogui/pyautogui-copy-file.txt"
+
+paste_site = "https://docs.google.com/document/d/1r7UHu9BukC1YtCJAQCw11JXtNsKh_fOFgxgn1hTvlGw/edit?usp=sharing"
+
+# Opening browser
+p.hotkey('win')
+p.sleep(2)
+p.write('brave')
+p.press('enter')
+time.sleep(3)
+
+# Entering site to copy
+p.write(copy_stite)
+time.sleep(1)
+p.press('enter')
+
+# Copying Data
+time.sleep(2)
+p.hotkey('ctrl', 'a')
+p.hotkey('ctrl', 'c')
+time.sleep(2)
+
+# Opening Google Docs
+p.hotkey('ctrl', 't')
+time.sleep(5)
+p.write(paste_site)
+time.sleep(1)
+p.press('enter')
+time.sleep(3)
+
+# Pasting the Data
+p.hotkey('ctrl', 'shift', 'v')
 
 time.sleep(1)
